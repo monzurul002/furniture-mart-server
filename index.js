908705913ce9501ba7e3f3ec216f7cc0f4ba314b
monzurul002 <monzurul002@gmail.com>
2024-03-14 06:48:26 +0600

    new route

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require("express");
 const app = express()
 const cors = require('cors')
@@ -26,16 +26,69 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+        //db collection name
         const furnituresCollection = client.db("furnitureMart").collection("allfurnitures")
+        const orderCollection = client.db("furnitureMart").collection("orderCollection")
+        const userCollection = client.db("furnitureMart").collection("users")
 
         //get user specific furniture
         app.get("/furniture", async (req, res) => {
-            const category = req.query.category;
-            const query = { category }
+            const { category } = req.query;
+            const searchQuery = category.toLowerCase()
+            const query = { category: searchQuery }
             const result = await furnituresCollection.find(query).toArray()
             res.send(result)
         })
+        // furniture get by id
+        app.get("/furniture/:id", async (req, res) => {
+            const { id } = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const result = await furnituresCollection.findOne(filter);
+            res.send(result);
+        })
 
+
+        //order furniture 
+        //post a furniture
+        app.post("/orders", async (req, res) => {
+            const furnitureInfo = req.body;
+            const result = orderCollection.insertOne(furnitureInfo);
+            res.send(result);
+
+        })
+        // get order collection
+        app.get("/orders", async (req, res) => {
+            const { email } = req.query;
+            const query = { userEmail: email }
+            const result = await orderCollection.find(query).toArray();
+            res.send(result)
+        })
+
+        app.put("/orders/:id", async (res, req) => {
+            const updatedInfo = req.body;
+            const { id } = req.params;
+            const filter = { _id: new ObjectId(id) }
+            const options = { upsert: true }
+            const updatedDoc = {
+                $set: {
+                    updatedInfo
+                }
+            }
+            const result = await orderCollection.updateOne(filter, updatedDoc, options);
+            res.send(result)
+        })
+        //  - ------------------------ user collection------------------------------ 
+        // post a user
+        app.post("/users", async (req, res) => {
+            const userInfo = req.body;
+            const result = await userCollection.insertOne(userInfo);
+            res.send(result)
+        })
+        app.get("/users", async (req, res) => {
+            const result = userCollection.find().toArray();
+            res.send(result)
+
+        })
 
 
         // Send a ping to confirm a successful connection
